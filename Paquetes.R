cba55d1cb8c851df8124e8e123d278a35e939ffa
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2018-07-04 22:25:07 -0500

    Slides en pdf

--- Paquetes.R
+++ Paquetes.R
@@ -1,4 +1,16 @@
 install.packages("ggplot2")
 install.packages("leaflet")
+install.packages("xaringan")
+install.packages("DT")
 library(leaflet)
 library(ggplot2)
+library(xaringan)
+library(DT)
+
+# Cómo imprimir una presentación en pdf
+
+install.packages("webshot")
+library(webshot)
+install_phantomjs()
+webshot("D:/Repositorios de GitHub/P_Xaringan/index.html", "mypdf.pdf")
+
